c4879b0c2b2b5bce2711bd2d09ac67249e56ca19
Noon Pokaratsiri <noonsky.noon@gmail.com>
2020-03-05 00:57:06 +0100

    preprocess.py done; tested fine on toy.txt

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# gsdmm
-CompLing Final Project GSDMM
+# Gibbs Samping Dirichlet Multinomial Model
+CompLing Final Project GSDMM Implementation as outlined in Yin and Wang (2014)
+To Do's
+- 2 data sets, 1 short texts and 1 long text
+    - Pang and Lee (2004) Movie review dataset
+    - 2015NAACL VSM-NLP workshop-"Short Text Clustering via Convolutional Neural Networks" dataset
+- pro-process corpora the same way; use the same pro-processing procedures as in the papers (Mazora; Yin amnd Wang)
+- Create a toy corpus for testing
+- Initialize
+- Write function to calculate the posterior probability for sampling new topic
+- Show words in documents of the final clusters
+- Show results/evaluate per metrics in the Y and W paper
---
+++ source_code/preprocess.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Preprocessing steps follow what is described in the paper and utilize the code from Shuyi / Cybozu Labs
+
+# This code is available under the MIT License.
+# (c)2010-2011 Nakatani Shuyo / Cybozu Labs Inc.
+
+import re
+import nltk
+
+
+def load_corpus(filename):
+    """
+
+    :param filename: path to corpus file
+    :return: list of lists of tokenized words in documents; each list is a document
+    """
+    tokenized_corpus = []
+    with open(filename, 'r') as f:
+        for line in f:
+            doc = re.findall(r'\w+(?:\'\w+)?', line)
+            # print(doc)
+            if len(doc) > 0:
+                tokenized_corpus.append(doc)
+    return tokenized_corpus
+
+
+stopwords_list = nltk.corpus.stopwords.words('english')
+recover_list = {'wa': 'was', 'ha': 'has'}
+word_lemmatizer = nltk.WordNetLemmatizer()
+
+
+def is_stopword(word):
+    """
+
+    :param word: a word token
+    :return: True or False
+    """
+    return word in stopwords_list
+
+
+def lemmatize(word):
+    """
+
+    :param word: a word token
+    :return: lemmatized word
+    """
+    w = word_lemmatizer.lemmatize(word.lower())
+    if w in recover_list:
+        return recover_list[w]
+    return w
+
+
+class Vocabulary:
+    def __init__(self, exclude_stopwords=True):
+        self.id_to_word = {}
+        self.word_to_id = {}
+        self.id_to_doc_frequency = {}  # maps word_id to number of documents the word(id) appears in
+        self.exclude_stopwords = exclude_stopwords
+
+    def term_to_id(self, word):
+        """
+
+        :param word: str word token
+        :return: int representation of a word token; None if non-alphabetical or stopword
+        """
+        term = lemmatize(word)
+        if not re.match(r'[a-z]+$', term):
+            return None
+        if self.exclude_stopwords and is_stopword(term):
+            return None
+        if term not in self.word_to_id.keys():
+            vocab_id = len(self.id_to_word)
+            self.word_to_id[term] = vocab_id
+            self.id_to_word[vocab_id] = term
+            self.id_to_doc_frequency[vocab_id] = 0
+        else:
+            vocab_id = self.word_to_id[term]
+        return vocab_id
+
+    def doc_to_ids(self, doc):
+        """
+
+        :param doc: list of string word tokens
+        :return: list of int representations of word tokens, excluding stopwords and non-alphabetical
+        """
+        doc_as_word_id_list = []
+        seen_word_ids = {}
+        for term in doc:
+            word_id = self.term_to_id(term)
+            if word_id is not None:
+                doc_as_word_id_list.append(word_id)
+                if word_id not in seen_word_ids.keys():
+                    seen_word_ids[word_id] = 1
+                    self.id_to_doc_frequency[word_id] += 1  # only increment doc count for each unique word id
+
+        return doc_as_word_id_list
+
+    def cut_low_freq(self, indexized_corpus, threshold=1):
+        """
+        update word_to_id, id_to_word, and id_to_doc_frequency to only contain words that appear at least above the
+        threshold
+
+        :param indexized_corpus: list of lists (docs) of integer-represented words
+        :param threshold: min number of docs word_id needs to appear in
+        :return: updated indexized corpus as list of lists (docs) with updated integer-represented words
+        """
+        new_id_to_word = {}
+        new_id_to_doc_freq = {}
+        self.word_to_id = {}
+        convert_old_id_to_new_id = {}
+        for word_id, term in self.id_to_word.items():
+            doc_freq = self.id_to_doc_frequency[word_id]  # num of docs word_id appears in
+            if doc_freq > threshold:
+                new_word_id = len(new_id_to_word)
+                self.word_to_id[term] = new_word_id
+                new_id_to_word[new_word_id] = term
+                new_id_to_doc_freq[new_word_id] = doc_freq
+                convert_old_id_to_new_id[word_id] = new_word_id
+        self.id_to_word = new_id_to_word
+        self.id_to_doc_frequency = new_id_to_doc_freq
+
+        def convert_word_ids(doc_of_word_ids):
+            new_doc = []
+            for old_word_id in doc_of_word_ids:
+                if old_word_id in convert_old_id_to_new_id:
+                    new_doc.append(convert_old_id_to_new_id[old_word_id])
+            return new_doc
+
+        return [convert_word_ids(doc) for doc in indexized_corpus]
+
+    def __getitem__(self, word_id):
+        return self.id_to_word[word_id]
+
+    def size(self):
+        return len(self.id_to_word)
+
+    def is_id_stopword(self, word_id):
+        return self.id_to_word[word_id] in stopwords_list
+
+
+def main():
+    toy = load_corpus('../data/toy.txt')
+    print(toy)
+
+    # process corpus
+    vocab = Vocabulary()
+    toy_docs = [vocab.doc_to_ids(doc) for doc in toy]
+    print(toy_docs)
+    print(vocab.id_to_doc_frequency)
+
+    toy_docs = vocab.cut_low_freq(toy_docs)
+    print(toy_docs)
+
+
+if __name__ == '__main__':
+    main()
+
